6ec18a279f6659504c367be99ad9e078d261e989
AswinAnand66 <86228052+AswinAnand66@users.noreply.github.com>
2023-06-20 22:47:52 +0530

    changes

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.css']
+  styleUrls: ['./rooms.component.css'],
 })
 export class RoomsComponent implements OnInit {
+  hotelname = 'Ananthya';
 
-  constructor() { }
+  rooms: Room = {
+    totalRooms:20,
+    availableRooms:10,
+    bookedRooms:20
+  };
 
-  ngOnInit(): void {
-  }
+ roomList: RoomList[] =[
 
+  {
+    roomType:'Deluxe Room',
+    amenities: 'Air conditioner',
+    price: 500,
+    photos: 'https://google.com',
+    checkinTime: new Date(),
+    checkoutTime: new Date()
+  },
+  {
+    roomType:'small Room',
+    amenities: 'Air conditioner',
+    price: 300,
+    photos: 'https://google.com',
+    checkinTime: new Date(),
+    checkoutTime: new Date()
+  },
+  {
+    roomType:'private Room',
+    amenities: 'Air conditioner',
+    price: 1000,
+    photos: 'https://google.com',
+    checkinTime: new Date(),
+    checkoutTime: new Date()
+  },
+ ]
+  
+  constructor() {}
+
+  ngOnInit(): void {}
 }
