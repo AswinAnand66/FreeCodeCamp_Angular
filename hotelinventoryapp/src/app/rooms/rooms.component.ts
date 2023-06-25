ee8c0bcd5c4379b8addac1ef67a86bde02e9accf
AswinAnand66 <86228052+AswinAnand66@users.noreply.github.com>
2023-06-25 23:00:03 +0530

    Added @Input and @Output

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -15,35 +15,57 @@ export class RoomsComponent implements OnInit {
     bookedRooms:20
   };
 
- roomList: RoomList[] =[
-
-  {
-    roomType:'Deluxe Room',
-    amenities: 'Air conditioner',
-    price: 500,
-    photos: 'https://google.com',
-    checkinTime: new Date(),
-    checkoutTime: new Date()
-  },
-  {
-    roomType:'small Room',
-    amenities: 'Air conditioner',
-    price: 300,
-    photos: 'https://google.com',
-    checkinTime: new Date(),
-    checkoutTime: new Date()
-  },
-  {
-    roomType:'private Room',
-    amenities: 'Air conditioner',
-    price: 1000,
-    photos: 'https://google.com',
-    checkinTime: new Date(),
-    checkoutTime: new Date()
-  },
- ]
+ roomList: RoomList[] =[ ]
   
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+    this.roomList =[
+
+      {
+        roomType:'Deluxe Room',
+        amenities: 'Air conditioner',
+        price: 500,
+        photos: 'https://google.com',
+        checkinTime: new Date(),
+        checkoutTime: new Date()
+      },
+      {
+        roomType:'small Room',
+        amenities: 'Air conditioner',
+        price: 300,
+        photos: 'https://google.com',
+        checkinTime: new Date(),
+        checkoutTime: new Date()
+      },
+      {
+        roomType:'private Room',
+        amenities: 'Air conditioner',
+        price: 1000,
+        photos: 'https://google.com',
+        checkinTime: new Date(),
+        checkoutTime: new Date()
+      },
+     ]
+      
+  }
+
+
+  selectedRoom(event:RoomList){
+    this.roomList.push(event);
+    console.log('event',event);
+  }
+
+  addRoom(){
+    let room = {
+      roomType:'private Room',
+      amenities: 'Air conditioner',
+      price: 1000,
+      photos: 'https://google.com',
+      checkinTime: new Date(),
+      checkoutTime: new Date()
+    }
+    this.roomList.push(room);
+  }
 }
